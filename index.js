283013d2ec7d3b66452a4ceeed708decb6a19eef
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-06-06 12:39:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,64 @@
-const express = require('express');
-const next = require('next');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-const port = process.env.PORT || 3000;
-
-app.prepare().then(() => {
-  const server = express();
-  const httpServer = http.createServer(server);
-  const io = socketIo(httpServer);
-
-  server.use(express.static(path.join(__dirname, 'public')));
-
-  let numUsers = 0;
-
-  io.on('connection', (socket) => {
-    let addedUser = false;
-
-    socket.on('new message', (data) => {
-      socket.broadcast.emit('new message', {
-        username: socket.username,
-        message: data
-      });
-    });
-
-    socket.on('add user', (username) => {
-      if (addedUser) return;
-
-      socket.username = username;
-      ++numUsers;
-      addedUser = true;
-      socket.emit('login', {
-        numUsers: numUsers
-      });
-      socket.broadcast.emit('user joined', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    });
-
-    socket.on('typing', () => {
-      socket.broadcast.emit('typing', {
-        username: socket.username
-      });
-    });
-
-    socket.on('stop typing', () => {
-      socket.broadcast.emit('stop typing', {
-        username: socket.username
-      });
-    });
-
-    socket.on('disconnect', () => {
-      if (addedUser) {
-        --numUsers;
-
-        socket.broadcast.emit('user left', {
-          username: socket.username,
-          numUsers: numUsers
-        });
-      }
-    });
-  });
-
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  httpServer.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Server listening at http://localhost:${port}`);
-  });
+server.listen(port, function() {
+	console.log('Server listening at port %d', port);
 });
+
+console.log('here');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var numUsers = 0;
+
+io.on('connection', function(socket) {
+	var addedUser = false;
+
+	socket.on('new message', function(data) {
+ 	socket.broadcast.emit('new message', {
+			username: socket.username,
+			message: data
+		});
+	});
+
+	socket.on('add user', function(username) {
+		if (addedUser) return;
+
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+		socket.broadcast.emit('user joined', {
+			username: socket.username,
+			numUsers: numUsers
+		});
+	});
+
+	socket.on('typing', function() {
+		socket.broadcast.emit('typing', {
+			username: socket.username
+		});
+	});
+
+	socket.on('stop typing', function() {
+		socket.broadcast.emit('stop typing', {
+			username: socket.username
+		});
+	});
+
+	socket.on('disconnect', function() {
+		if (addedUser) {
+			--numUsers;
+
+			socket.broadcast.emit('user left', {
+				username: socket.username,
+				numUsers: numUsers
+			});
+		}
+	});
+});     
